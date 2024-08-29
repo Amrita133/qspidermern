e0112521b58d3ba5ff861f6324423fe3c1a5daa2
Amrita Vishwakarma <amrita9463360@gmail.com>
2024-08-29 12:04:06 +0530

    js

---
+++ Js/function/script.js
@@ -0,0 +1,82 @@
+/*function callSalman(name){
+    console.log("Hud hud dabang!!")
+    return `${name},woh sirf mwri h..!`
+}
+
+let result=callSalman("Katrina")
+console.log (result)
+*/
+
+
+/*function call(a,b){
+    let sum=a+b;
+    return sum; 
+}
+console.log(call(4,6))
+*/
+
+
+
+/* function nam(){
+    console.log("Hey Amrita..")
+}
+nam()
+*/
+
+
+//1.calculate sum of tow number
+/*function findSum(a,b){
+    let sum=a+b;
+    return sum;
+}
+let result=findSum(10,10)
+console.log(`Result is:${result}`)
+// console.log(findSum(10,10))
+*/
+
+
+
+//2...
+// function task(name){
+/*function task(name="abc"){
+    console.log(`User ${name} Logged in...`)
+}
+//task("Amri")   ...used name for call the function
+// task() ..returns undefined at the place of name  
+
+task("xyz")*/
+
+
+
+// 3....
+/*function taskTwo(name="Amrita"){
+    name!=="Amrita"?console.log(`user ${name} logged in..`):
+    console.log("Invalid..")
+}
+// taskTwo("surya")  
+taskTwo("Amrita")
+*/
+
+
+/*function task(){
+    console.log(`user name is ${arguments[0]} `);
+    console.log(`Age is ${arguments[1]}`)
+}
+task("Amrii",23)   //this value are the arguments
+//agument can access inside the {} brackets of the function
+//when we don't have parameter [task()]  then we use arguments for access the values
+
+*/
+
+
+
+let a=1;
+let b=10;
+
+function product(x,y){
+    var prod=x*y;
+    return prod;
+}
+product(a,b)
+
+
